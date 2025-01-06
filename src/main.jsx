8858522f22b000ced9d1207dc5cbca7c71c897a5
Echo271 <erdarmawan7@gmail.com>
2025-01-06 16:12:04 +0800

    add icons and font css files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/css/index.css'
-import './assets/css/icon.css'
+import './assets/css/icons.css'
+import './assets/css/font.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
